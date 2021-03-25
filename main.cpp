2518e911346034dc3783d63945ff366f38473f31
zhujiaying <zjy_1997@126.com>
2021-03-25 19:45:56 +0800

    clean up main.cpp

--- main.cpp
+++ main.cpp
@@ -2,107 +2,15 @@
 
 #include "TopFlow.h"
 #include "BasicBlock.h"
-#include "Blocks/NullSource.h"
 #include "Blocks/NullSink.h"
-#include "Blocks/Proc.h"
-#include "Sources/sig_source.h"
-#include "Blocks/mi_channel.h"
-#include "Blocks/node_tag.h"
 #include "Blocks/MsgGenerator.h"
 #include "Blocks/ConvertByteBit.h"
 #include "Blocks/BPSK.h"
 #include "Blocks/BPSKDemod.h"
 #include "Blocks/AWGNChannel.h"
 #include "Blocks/BER.h"
-#include <complex>
-#include <ctime>
-#include <iostream>
 
 typedef std::shared_ptr<BasicBlock> spBasicBlock;
-typedef std::shared_ptr<node_tag> spNode;
-
-typedef std::complex<float> lv_32fc_t;
-
-void volk_32fc_x2_dot_prod_32fc_generic(lv_32fc_t* result,
-                                      const lv_32fc_t* input,
-                                      const lv_32fc_t* taps,
-                                      unsigned int num_points)
-{
-
-    float* res = (float*)result;
-    float* in = (float*)input;
-    float* tp = (float*)taps;
-    unsigned int n_2_ccomplex_blocks = num_points / 2;
-
-    float sum0[2] = { 0, 0 };
-    float sum1[2] = { 0, 0 };
-    unsigned int i = 0;
-
-    for (i = 0; i < n_2_ccomplex_blocks; ++i) {
-        sum0[0] += in[0] * tp[0] - in[1] * tp[1];
-        sum0[1] += in[0] * tp[1] + in[1] * tp[0];
-        sum1[0] += in[2] * tp[2] - in[3] * tp[3];
-        sum1[1] += in[2] * tp[3] + in[3] * tp[2];
-
-        in += 4;
-        tp += 4;
-    }
-
-    res[0] = sum0[0] + sum1[0];
-    res[1] = sum0[1] + sum1[1];
-
-    // Cleanup if we had an odd number of points
-    if (num_points & 1) {
-        *result += input[num_points - 1] * taps[num_points - 1];
-    }
-}
-
-void compute_optimize(lv_32fc_t* result, const lv_32fc_t* input, const lv_32fc_t* taps, unsigned int num_points)
-{
-    float* res = (float*)result;
-    float* in = (float*)input;
-    float* tp = (float*)taps;
-
-    float sum0[2] = {0,0};
-    unsigned int i = 0;
-    for (i=0; i<num_points; ++i)
-    {
-        sum0[0] += in[0] * tp[0] - in[1] * tp[1];
-        sum0[1] += in[0] * tp[1] + in[1] * tp[0];
-        in += 2;
-        tp += 2;
-    }
-    res[0] = sum0[0];
-    res[1] = sum0[1];
-}
-
-void test_compute_optimize()
-{
-    const unsigned int num = 4096;
-    lv_32fc_t input[num];
-    lv_32fc_t taps[num];
-    lv_32fc_t result;
-    for (int i=0; i<num; ++i)
-    {
-        input[i] = {(float)(i%5)/(float)5.0, (float)(i%7)/(float)6.0};
-        taps[i] = {(float)(i%10)/(float)3.0, (float)(i%8)/(float)8.0};
-    }
-
-    clock_t start,end;
-    start = clock();
-    compute_optimize(&result, input, taps, num);
-    end = clock();
-    double normaltime = (double)(end-start)/CLOCKS_PER_SEC;
-    std::cout << "normal result: " << result << std::endl;
-    std::cout << "normal time: " << normaltime*1000 << "ms" << std::endl;
-
-    start = clock();
-    volk_32fc_x2_dot_prod_32fc_generic(&result, input, taps, num);
-    end = clock();
-    double volktime = (double)(end-start)/CLOCKS_PER_SEC;
-    std::cout << "volk result: " << result << std::endl;
-    std::cout << "volk time: " << volktime*1000 << "ms" << std::endl;
-}
 
 void tests()
 {
