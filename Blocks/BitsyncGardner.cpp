b3578326a5afcc4599ac7bea7efc0ce10cb007da
zhujiaying <zjy_1997@126.com>
2021-04-27 15:32:33 +0800

    Update 2021.4.27 增加了降采样、位同步，修改了帧同步模块。

--- Blocks/BitsyncGardner.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by zhujiaying on 2021/4/26.
-//
-
-#include "BitsyncGardner.h"
-
-BitsyncGardner::BitsyncGardner()
-{}
---
+++ VectorCalc.h
@@ -0,0 +1,13 @@
+//
+// Created by zhujiaying on 2021/4/27.
+//
+
+#ifndef SIMPLATFORM_VECTORCALC_H
+#define SIMPLATFORM_VECTORCALC_H
+
+float f_prod_f(float* x, float* y)
+{
+
+}
+
+#endif //SIMPLATFORM_VECTORCALC_H
